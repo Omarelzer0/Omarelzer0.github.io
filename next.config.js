88c51f87791a40d62a2a62db3d975db44d3539ab
muztafa74 <you@example.commuztafalap@gmail.com>
2024-09-26 16:29:18 +0300

    Describe your update briefy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      RESEND_API_KEY: process.env.RESEND_API_KEY,
+      FROM_EMAIL: process.env.FROM_EMAIL,
+    },
+  }
+  
+  module.exports = nextConfig
