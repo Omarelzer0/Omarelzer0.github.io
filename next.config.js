cfb93bc7a9cd8a9ebd78907e1dbe32d743807596
omar <omar.bn.hassan89@gmail.com>
2025-02-03 21:25:52 +0200

    "Prepare for GitHub Pages deployment"

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
+const repoName = 'Omarelzer0.github.io'; // استبدل Omarelzer0.github.io باسم المستودع الخاص بك
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProduction ? `/${repoName}` : '', // إضافة assetPrefix
+  basePath: isProduction ? `/${repoName}` : '', // إضافة basePath
+  trailingSlash: true, // إضافة trailingSlash
+  output: 'export', // إضافة output: export
 };
 
 module.exports = nextConfig;
