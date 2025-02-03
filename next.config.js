3ddc2c8ec44aac6feadde3019e4301fdfd7094da
omar <omar.bn.hassan89@gmail.com>
2025-02-03 22:07:21 +0200

    for vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production';
-const repoName = 'Omarelzer0.github.io'; // استبدل Omarelzer0.github.io باسم المستودع الخاص بك
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProduction ? `/${repoName}` : '', // إضافة assetPrefix
-  basePath: isProduction ? `/${repoName}` : '', // إضافة basePath
-  trailingSlash: true, // إضافة trailingSlash
-  output: 'export', // إضافة output: export
+  // إزالة الإعدادات الخاصة بـ GitHub Pages
+  // assetPrefix: isProduction ? `/${repoName}` : '',
+  // basePath: isProduction ? `/${repoName}` : '',
+  // trailingSlash: true,
+  // output: 'export', // إزالة هذا السطر إذا كنت تريد استخدام وظائف ديناميكية
 };
 
 module.exports = nextConfig;
